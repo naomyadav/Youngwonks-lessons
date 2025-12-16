9404d5a820f61f35cc43dd2782ac6cc5346c4ff4
naomyadav <yadavnaom@gmail.com>
2025-12-15 19:42:50 -0800

    modified:   python_challenges_with_tests_original.py

--- python_challenges_with_tests_original.py
+++ python_challenges_with_tests_original.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 """
 Python Coding Challenges: Core Concepts Practice (30 Problems)
@@ -578,16 +579,16 @@ def floyds_triangle(n):
     """
     sum_last_num_row = 0
     #Write code here
-    #I ANGRY I MAKE pseudo code 😡
-    """
-    
-    sum_last_row=0
-    rotation_num=0
-    for i in row.get(n):
-       sum_last_row+=i
-       rotation_num+=1 
-    
-    """
+    print()
+    a=1
+    for i in range(1,n+1):
+        for n in range(i):
+            print(a,end=" ")
+            a+=1
+        print()
+
+    print()
+    sum_last_num_row=a-1
     return sum_last_num_row
 
 # 28. Math Practice
@@ -614,11 +615,21 @@ def generate_password(length=15):
     Returns:
     - A string representing the password
     """
-    gen_passwd = "Abc123simeballs"
+    chars=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    uppercase_letters=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    digits=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    lists=[chars,uppercase_letters,digits]
+    #one_list = 
+    gen_passwd = ""
     gen_passwd_lst=list(gen_passwd)
-    if length > 15:
-        for i in range(length-15):
-            gen_passd_lst.append()
+    gen_passwd_lst.append(random.choice(chars))
+    gen_passwd_lst.append(random.choice(uppercase_letters))
+    gen_passwd_lst.append(random.choice(digits))
+    #if length > 15:
+    for i in range(length):
+        gen_passwd_lst.append(random.choice(random.choice(lists)))
+    gen_passwd="".join(gen_passwd_lst)
+    print(gen_passwd)
     #Write code here
     
     return gen_passwd
